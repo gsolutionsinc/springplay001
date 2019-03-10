a31c0620395f0789040faca63f5bda55a56d1948
Sandeep Giri <giri.sandeep.k@gmail.com>
2019-03-10 17:21:24 -0400

    clean url and lombok slf4j logging
    lombok slf4j logging inclusion
    Clean url using @PathVariable annotation

--- src/main/java/com/gsol/springplay001/controller/UserController.java
+++ src/main/java/com/gsol/springplay001/controller/UserController.java
@@ -10,6 +10,7 @@ import com.gsol.springplay001.service.UserServiceI;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,21 +27,29 @@ public class UserController {
 
     @GetMapping(path="/ping", produces = "application/json")
     public String ping() {
+
+        log.info("--ping--");
         return new Date().toString();
     }
 
-    @GetMapping(path="/userdetails", produces = "application/json")
-    public User userDetails(int userId) {
+    @GetMapping(path="/userdetails/{userId}", produces = "application/json")
+    public User userDetails(@PathVariable int userId) {
+
+        log.info("--userDetails--");
         return userService.findUserAllergentList(userId);
     }
 
     @GetMapping(path = "/allusers", produces = "application/json")
     public List<User> allUsers() {
+
+        log.info("--allUsers--");
         return userService.findAllUser();
     }
 
-    @GetMapping(path="/userfoodallergies", produces = "application/json")
-    public List<FoodAllergent> userFoodAllergies(int userId) {
+    @GetMapping(path="/userfoodallergies/{userId}", produces = "application/json")
+    public List<FoodAllergent> userFoodAllergies(@PathVariable int userId) {
+
+        log.info("--userFoodAllergies--");
         return userService.findUserAllergentList(userId).getFoodAllergentsList();
     }
 
